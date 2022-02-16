8c613620bf5d1a4e6a910931843030ae2d783163
Not-Mango <jackhb@hotmail.co.uk>
2022-02-16 20:09:49 +0000

    WrathOfZeusEnchantment Added

---
+++ src/main/java/net/notmango/mangotechores/enchantment/ModEnchantment.java
@@ -0,0 +1,24 @@
+package net.notmango.mangotechores.enchantment;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.notmango.mangotechores.MangoTechOres;
+
+public class ModEnchantment {
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS
+            = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, MangoTechOres.MOD_ID);
+
+    public static RegistryObject<Enchantment> WRATH_OF_ZEUS =
+            ENCHANTMENTS.register("wrath_of_zeus",
+                    () -> new WrathOfZeusEnchantment(Enchantment.Rarity.RARE,
+                            EnchantmentCategory.BOW, EquipmentSlot.MAINHAND));
+
+    public static void register(IEventBus eventBus) {
+        ENCHANTMENTS.register(eventBus);
+    }
+}
